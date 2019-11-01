e032b6d2459c04c054a771f34e9e81d735ea39b4
Juliano Sirtori <julianosirtori@gmail.com>
2019-11-01 05:20:53 -0300

    Add Emmer

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,7 +1,19 @@
+import produce from 'immer';
+
 export default function cart(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [...state, action.product];
+      return produce(state, draf => {
+        const productIndex = draf.findIndex(p => p.id === action.product.id);
+        if (productIndex >= 0) {
+          draf[productIndex].amount += 1;
+        } else {
+          draf.push({
+            ...action.product,
+            amount: 1,
+          });
+        }
+      });
     default:
       return state;
   }
