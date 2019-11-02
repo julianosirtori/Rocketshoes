26e12d41b90a14c81df71eea88c7239b93d6713e
Juliano Sirtori <julianosirtori@gmail.com>
2019-11-02 07:04:13 -0300

    Modulo07 Finished

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -4,15 +4,8 @@ export default function cart(state = [], action) {
   switch (action.type) {
     case '@cart/ADD_SUCESS':
       return produce(state, draf => {
-        const productIndex = draf.findIndex(p => p.id === action.product.id);
-        if (productIndex >= 0) {
-          draf[productIndex].amount += 1;
-        } else {
-          draf.push({
-            ...action.product,
-            amount: 1,
-          });
-        }
+        const { product } = action;
+        draf.push(product);
       });
     case '@cart/REMOVE':
       return produce(state, draf => {
@@ -21,10 +14,7 @@ export default function cart(state = [], action) {
           draf.splice(productIndex, 1);
         }
       });
-    case '@cart/UPDATE_AMOUNT': {
-      if (action.amount <= 0) {
-        return state;
-      }
+    case '@cart/UPDATE_AMOUNT_SUCESS': {
       return produce(state, draf => {
         const productIndex = draf.findIndex(p => p.id === action.id);
         if (productIndex >= 0) {
